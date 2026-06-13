777aeaaebab19e8aeb77b4260183cb6d5b8cccdb
luismtapiab <lmtapia1024@gmail.com>
2026-06-12 21:18:25 -0400

    testing the compose.yaml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1> <h2>by Luis Tapia</h2>')
+  res.send('<h1>Hello World!</h1> <h2>by Luis Tapia</h2> <p> I had set up a CI/CD pipeline with gh actions and Watchtower</p>')
 })
 
 const PORT = 8080
